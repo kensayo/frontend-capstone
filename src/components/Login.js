eaadcfdb9d1b5a28277b306f2d43120103bb5b63
SunnySparks <Josediazt6@outlook.com>
2022-01-14 17:50:47 -0600

    Center submit button

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,21 @@
 import '../App.css';
+import './login.css';
 
 const Login = () => (
   <div className="logIn d-flex flex-column justify-content-center align-items-center">
-    <div className="header">
+    <div className="header p-5">
       <h1>Log In</h1>
       <p>Hello there! Sign in here</p>
     </div>
-    <div className="form">
+    <div className="form p-5">
       <form>
-        <input type="text" name="email" />
-        <input type="submit" value="Submit" />
+        <div className="flex-column">
+          <input className="row logName" type="text" name="email" />
+          <input className="row submitBtn" type="submit" value="Submit" />
+        </div>
       </form>
     </div>
-    <div className="forgot">
+    <div className="forgot p-5">
       <p>Forgot Password?</p>
     </div>
   </div>
