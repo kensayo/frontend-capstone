3561a48d81e6b4abca001e830840270ed1f6837e
SunnySparks <Josediazt6@outlook.com>
2022-01-14 19:11:23 -0600

    Finish login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,19 +3,23 @@ import './login.css';
 
 const Login = () => (
   <div className="logIn d-flex flex-column justify-content-center align-items-center">
-    <div className="header p-5">
+    <div className="header text-white p-5">
       <h1>Log In</h1>
       <p>Hello there! Sign in here</p>
     </div>
-    <div className="form p-5">
-      <form>
+    <div className="form">
+      <form className="input-group">
         <div className="flex-column">
-          <input className="row logName" type="text" name="email" />
-          <input className="row submitBtn" type="submit" value="Submit" />
+          <div className="flex-column p-2">
+            <input className="row logName" type="text" name="email" />
+          </div>
+          <div className="flex-column p-2">
+            <input className="row submitBtn" type="submit" value="Submit" />
+          </div>
         </div>
       </form>
     </div>
-    <div className="forgot p-5">
+    <div className="forgot text-white p-5">
       <p>Forgot Password?</p>
     </div>
   </div>
