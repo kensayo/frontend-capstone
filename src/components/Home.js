9cf86e22b004d252d6d53a68ed189a763ac873ea
Zulma Rocio <zrmartinezg@gmail.com>
2022-01-17 16:49:51 -0500

    Add fontawesome

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <nav>
+      <i className="fa fa-spinner fa-spin" />
+    </nav>
+  </div>
+);
+
+export default Home;
--- src/components/Routing.js
+++ src/components/Routing.js
@@ -2,13 +2,15 @@ import {
   BrowserRouter as Router, Routes,
 } from 'react-router-dom';
 import { Route } from 'react-router';
+import Home from './Home';
 import Login from './Login';
 import Signup from './Signup';
 
 const Routing = () => (
   <Router>
     <Routes>
-      <Route exact path="/" element={<Login />} />
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/login" element={<Login />} />
       <Route exact path="/signup" element={<Signup />} />
     </Routes>
   </Router>
