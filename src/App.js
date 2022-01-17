d2f1a00074d0652fc518b6af308f31ebab5168e5
Zulma Rocio <zrmartinezg@gmail.com>
2022-01-17 15:41:49 -0500

    Import bootstrap in index js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
 import Routing from './components/Routing';
 
 function App() {
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <React.StrictMode>
