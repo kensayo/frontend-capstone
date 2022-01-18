daa163b916e0a8916665d3ab27afa0f86f0a5ea7
SunnySparks <Josediazt6@outlook.com>
2022-01-17 20:26:13 -0600

    Install fontawesome and add back icon to Favorite page

--- src/components/Favorite.js
+++ src/components/Favorite.js
@@ -1,5 +1,35 @@
-const Favorite = () => (
-  <div />
-);
+import '../App.css';
+import { React, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+import { getFavorites } from '../redux/favorite/favorite';
+
+const Favorite = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getFavorites());
+  }, []);
+
+  const favList = useSelector((state) => state.favoritesReducer);
+  console.log(favList);
+  return (
+    <div>
+      holi
+      <Link
+        className="nav-link"
+        to="/"
+      >
+        <div className="row pl-2 text-dark">
+          <h2 className="links-nav col">
+            <FontAwesomeIcon icon={faArrowLeft} />
+          </h2>
+        </div>
+      </Link>
+    </div>
+  );
+};
 
 export default Favorite;
