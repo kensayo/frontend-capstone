1625429a7defe30bcb663d1482fc7491f9873ca6
SunnySparks <Josediazt6@outlook.com>
2022-01-17 22:28:54 -0600

    initial layout for favorites page

--- src/components/Favorite.js
+++ src/components/Favorite.js
@@ -1,8 +1,9 @@
 import '../App.css';
+import './favorite.css';
 import { React, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { faArrowLeft, faHeart, faStar } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 import { getFavorites } from '../redux/favorite/favorite';
 
@@ -17,17 +18,58 @@ const Favorite = () => {
   console.log(favList);
   return (
     <div>
-      holi
-      <Link
-        className="nav-link"
-        to="/"
-      >
-        <div className="row pl-2 text-dark">
-          <h2 className="links-nav col">
-            <FontAwesomeIcon icon={faArrowLeft} />
-          </h2>
+      <div className="topContr row">
+        <div>
+          <Link
+            className="nav-link"
+            to="/"
+          >
+            <div className="row pl-2 text-dark">
+              <h2 className="links-nav col">
+                <FontAwesomeIcon icon={faArrowLeft} />
+              </h2>
+            </div>
+          </Link>
         </div>
-      </Link>
+        <div>
+          <h2>Favorites</h2>
+        </div>
+        <div>
+          <h4>Menu</h4>
+        </div>
+      </div>
+      <div className="previewCont d-flex justify-content-around align-items-end">
+        <div className="infoCont row">
+          <div className="info col row">
+            <div className="col-6">
+              <div className="processorPic" />
+            </div>
+            <div className="col-6">
+              <h4>All in one LENOVO</h4>
+              <p>
+                <FontAwesomeIcon icon={faStar} />
+                <FontAwesomeIcon icon={faStar} />
+                <FontAwesomeIcon icon={faStar} />
+                <FontAwesomeIcon icon={faStar} />
+                <FontAwesomeIcon icon={faStar} />
+              </p>
+            </div>
+          </div>
+          <div className="price col">
+            <h4>$10</h4>
+            <p>Per Day</p>
+          </div>
+        </div>
+      </div>
+      <div className="descriptionCont">
+        <h4>About</h4>
+        <p>TextTextTextTextTextTextTextTextTextText</p>
+      </div>
+      <div className="orange">
+        <h2 className="links-nav col text-white">
+          <FontAwesomeIcon icon={faHeart} />
+        </h2>
+      </div>
     </div>
   );
 };
