23984d1787003ecc1934195a5c543d7ee8fe4aa1
SunnySparks <Josediazt6@outlook.com>
2022-01-18 17:34:10 -0600

    Create Actions and reducer for computers(items)

---
+++ src/redux/items/item.js
@@ -0,0 +1,30 @@
+// http://localhost:3000/api/v1/computers
+import axios from 'axios';
+
+const GET_COMPUTERS = 'GET_COMPUTERS';
+const API_URL = 'http://localhost:3001/api/v1/computers';
+
+const initialState = [];
+
+export const getComputers = () => async (dispatch) => {
+  await axios.get(API_URL, {
+    method: 'GET',
+  })
+    .then((response) => {
+      dispatch({
+        type: GET_COMPUTERS,
+        payload: response,
+      });
+    });
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_COMPUTERS:
+      console.log(action.payload.data);
+      return action.payload.data;
+    default:
+      return state;
+  }
+};
+export default reducer;
