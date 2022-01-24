4f16bb666d7ed9bed3be2ad7933f24c03a598917
SunnySparks <Josediazt6@outlook.com>
2022-01-24 15:50:19 -0600

    Fix menu coloring

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,18 +4,20 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => (
   <div>
-    <div className="cont">
+    <div className="cont mt-2 mb-2">
       <div className="navigation">
         <input type="checkbox" className="toggle-menu" />
         <div className="hamburger" />
-        <div className="logo">
-          <div>
-            <p>
-              dofjafkhasfjkdgfjkasfgakjasgfkashfgak
-            </p>
-          </div>
-        </div>
         <ul className="menu">
+          <li>
+            <div className="logo">
+              <div>
+                <p>
+                  dofjafkhasfjkdgfjkasfgakjasgfkashfgak
+                </p>
+              </div>
+            </div>
+          </li>
           <li><Link to="/fav" className="navLink">Favorites</Link></li>
           <li><Link to="/login" className="navLink">Login</Link></li>
           <li><Link to="/signup" className="navLink">Signup</Link></li>
