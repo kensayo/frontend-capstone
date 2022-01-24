2dff73cee4a2cd7401255bf8ab1d4deeb371ee8a
SunnySparks <Josediazt6@outlook.com>
2022-01-24 14:10:36 -0600

    Finish navbar layout and animations

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,24 +2,27 @@ import '../App.css';
 import './navbar.css';
 import { Link } from 'react-router-dom';
 
-const Navbar = () => {
-  const handleClick = () => {
-    console.log('click');
-  };
-
-  return (
-    <div>
-      <button className="hamburger is-active" type="button" onClick={handleClick()}>
-        <div className="bar" />
-      </button>
-      <nav className="is-active">
-        <Link to="/" className="navLink">Home</Link>
-        <Link to="/fav" className="navLink">Favorites</Link>
-        <Link to="/login" className="navLink">Login</Link>
-        <Link to="/signup" className="navLink">Signup</Link>
-      </nav>
+const Navbar = () => (
+  <div>
+    <div className="cont">
+      <div className="navigation">
+        <input type="checkbox" className="toggle-menu" />
+        <div className="hamburger" />
+        <div className="logo">
+          <div>
+            <p>
+              dofjafkhasfjkdgfjkasfgakjasgfkashfgak
+            </p>
+          </div>
+        </div>
+        <ul className="menu">
+          <li><Link to="/fav" className="navLink">Favorites</Link></li>
+          <li><Link to="/login" className="navLink">Login</Link></li>
+          <li><Link to="/signup" className="navLink">Signup</Link></li>
+        </ul>
+      </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default Navbar;
