de7acd0576e6725ffaa0d92f86e0b2f6ec7d5585
Zulma Rocio <zrmartinezg@gmail.com>
2022-01-24 18:18:57 -0500

    Add handleSubmit on Signup component

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
-// import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import '../App.css';
 import './login.css';
+import { signupUser } from '../redux/users/user';
 
 const Signup = () => {
   const [username, setUserName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
   const handleUserName = (e) => {
     setUserName(e.target.value);
@@ -23,6 +24,24 @@ const Signup = () => {
     setPassword(e.target.value);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const user = {
+      username,
+      email,
+      password,
+    };
+
+    dispatch(signupUser(user));
+
+    setUserName('');
+    setEmail('');
+    setPassword('');
+  };
+
+  // const signedinUser = useSelector((state) => state.user);
+
   return (
     <div className="logIn d-flex flex-column justify-content-center align-items-center">
       <div className="header text-white p-5">
@@ -30,7 +49,7 @@ const Signup = () => {
         <p>Create an account here</p>
       </div>
       <div className="form">
-        <form className="input-group">
+        <form className="input-group" onSubmit={handleSubmit}>
           <div className="flex-column">
             <div className="flex-column p-2">
               <input className="row logName" type="text" onChange={handleUserName} value={username} placeholder="username" />
